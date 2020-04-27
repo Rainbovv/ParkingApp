d6390aed9303ea37e9816ac5b9b885b843273cd7
Radu Ţurcanu <63318406+Rainbovv@users.noreply.github.com>
2020-04-27 05:08:35 +0300

    Add files via upload

---
+++ src/src/Application.java
@@ -0,0 +1,14 @@
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Parking p1 = new Parking();
+		Car c1 = new Car("BMW", "AB-123");
+		Car c2 = new Car("Fiat", "xy-567");
+		p1.parkCar(c1, "1a");
+		p1.parkCar(c2, "2c");
+		p1.printMap();
+		p1.printInfoPlace("1a");
+	}
+		
+}
